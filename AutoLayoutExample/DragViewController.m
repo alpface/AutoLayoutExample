af242e73b48aea54075ac3e807bf4c834f5df9c3
xiaoyuan <392237716@qq.com>
2018-03-15 15:43:40 +0800

    Update drag example

--- AutoLayoutExample/DragViewController.m
+++ AutoLayoutExample/DragViewController.m
@@ -37,7 +37,7 @@
     id topLayoutGuide = self.topLayoutGuide;
     NSDictionary *viewDict = @{@"button1": self.button1, @"button2": self.button2, @"topLayoutGuide": topLayoutGuide};
     // 添加这两个button相对父控件的垂直约束，并且这两个控件的中心点x轴对齐NSLayoutFormatAlignAllCenterX(也就是垂直对齐)
-    [NSLayoutConstraint activateConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[topLayoutGuide]-(10.0)-[button1]-(==padding)-[button2(>=0)]->=0-|" options:NSLayoutFormatAlignAllCenterX metrics:@{@"padding": @(padding)} views:viewDict]];
+    [NSLayoutConstraint activateConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[topLayoutGuide]-(padding)-[button1]-(==padding)-[button2(>=0)]->=0-|" options:NSLayoutFormatAlignAllCenterX metrics:@{@"padding": @(padding)} views:viewDict]];
     // 然后只需要确定好这两个button中其中一个的中心点x轴相对布局即可
     [NSLayoutConstraint constraintWithItem:self.button1 attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0].active = YES;
 }
@@ -51,25 +51,42 @@
     CGPoint translation = [pan translationInView:self.view];
     CGPoint translationCenter = CGPointMake(translation.x + pan.view.center.x,
                                             translation.y + pan.view.center.y);
+    CGPoint panViewCenter = [self.view convertPoint:pan.view.center toView:self.view];
     // 重置translation
     [pan setTranslation:CGPointZero inView:self.view];
     
     switch (pan.state) {
         case UIGestureRecognizerStateChanged: {
-            [UIView animateWithDuration:0.10 animations:^{
+//            [UIView animateWithDuration:0.10 animations:^{
                 /// @note: 由于此处是更新视图的中心点坐标，通过AutoLayout的约束不会受影响，所以在下一次重新布局时(比如执行layoutIfNeeded)时，中心点坐标将会被恢复为约束时的布局
                 pan.view.center = translationCenter;
-            }];
+//            }];
             break;
         }
-        case UIGestureRecognizerStateEnded: {
-            // 手势结束时恢复布局
-            [UIView animateWithDuration:0.5 delay:0.0 usingSpringWithDamping:0.3 initialSpringVelocity:5.0 options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut animations:^{
-                [self.view setNeedsLayout];
-                [self.view layoutIfNeeded];
+        case UIGestureRecognizerStateEnded:
+        case UIGestureRecognizerStateCancelled:
+        case UIGestureRecognizerStateFailed: {
+            // 计算速度向量的长度，当他小于200时，滑行会很短
+            CGPoint velocity = [pan velocityInView:self.view];
+            // 计算速度向量的长度
+            CGFloat magnitude = sqrtf((velocity.x * velocity.x) + (velocity.y * velocity.y));
+            CGFloat slideMult = magnitude / 200;
+            // 基于速度和速度因素计算一个终点
+            float slideFactor = 0.1 * slideMult;
+            CGPoint finalPoint = CGPointMake(panViewCenter.x + (velocity.x * slideFactor),  panViewCenter.y + (velocity.y * slideFactor));
+            [UIView animateWithDuration:0.30 animations:^{
+                /// @note: 由于此处是更新视图的中心点坐标，通过AutoLayout的约束不会受影响，所以在下一次重新布局时(比如执行layoutIfNeeded)时，中心点坐标将会被恢复为约束时的布局
+                pan.view.center = finalPoint;
             } completion:^(BOOL finished) {
-                
+                // 手势结束时恢复布局
+                [UIView animateWithDuration:0.5 delay:0.0 usingSpringWithDamping:0.3 initialSpringVelocity:10.0 options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut animations:^{
+                    [self.view setNeedsLayout];
+                    [self.view layoutIfNeeded];
+                } completion:^(BOOL finished) {
+                    
+                }];
             }];
+            
             break;
         }
         default:
