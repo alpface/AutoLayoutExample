8c19b83ac0da1d2bbc6e9e48e59202f199892784
xiaoyuan <392237716@qq.com>
2018-03-20 17:58:54 +0800

    Update property

--- AutoLayoutExample/NaviActionContentView.h
+++ AutoLayoutExample/NaviActionContentView.h
@@ -38,7 +38,7 @@
 @property (nonatomic, strong) NSMutableArray<NaviActionButton *> *buttonArray;
 @property (nonatomic, strong) NSMutableArray<NaviActionItem *> *items;
 // item高度
-@property (nonatomic, assign) IBInspectable CGFloat itemHeight;
+@property (nonatomic, assign) CGFloat itemHeight;
 /// 每行item的个数, 此方法会根据设置的间距itemHPadding值，自适应item的宽度, 最小为1
 @property (nonatomic, assign) IBInspectable NSUInteger maxNumberOfLine;
 /// 正方形，设置此属性后，宽高相同
@@ -48,7 +48,7 @@
 /// 当为YES时，不管这一行有多少个都是平分行的宽度，反之，不自适应宽度
 /// 如果只有一行且数量不足maxNumberOfLine时也是会自适应宽度
 /// default is NO
-@property (nonatomic, assign, getter=isSizeAdaptive) BOOL sizeAdaptive;
+@property (nonatomic, assign, getter=isSizeAdaptive) IBInspectable BOOL sizeAdaptive;
 @property (nonatomic, assign) IBInspectable CGFloat itemHPadding;
 @property (nonatomic, assign) IBInspectable CGFloat itemVPadding;
 @property (nonatomic, strong) NSMutableArray<NSLayoutConstraint *> *viewConstraints;
